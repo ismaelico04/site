115c51d91283c66f15e464fff3833068ce78a5d0
ismaelico04 <129362932+ismaelico04@users.noreply.github.com>
2024-02-13 14:13:45 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+//var anuncio = alert ("Hola muy buenas") // Esto va a mostrar en la pagina "Hola muy buenas"
+
+typeof dogId === "string" // true
+
+/* if (condicion) {
+    //entre llaves este codigo
+    //se ejecuta si la condicion es true
+} */
+
+/* const edad = 20
+
+if(edad >= 18) {
+    console.log("Es mayor de edad")
+} else if (edad === 0) {
+    console.log("Recien nacido")
+}else {
+    console.log("es menor de edad")
+}  */
+
+/* while (condition) { // cuando la condition sea false no se ejecutara el codigo, si es true se ejecuta lo que haya dentro del codigo
+    //console.log
+} */
+
+let cuentaAtras = 10
+
+while (cuentaAtras > 0) {
+    cuentaAtras = cuentaAtras -1
+    console.log(cuentaAtras + ' segundos restantes')
+}
+
+const sum = (a,b)
